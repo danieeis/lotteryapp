51ade05110762149fcf3190634da8d750959aeb3
Daniel Tovar <danieldaniyyelda@gmail.com>
2021-03-03 03:27:03 -0400

    fix build on android

--- src/FloridaLottery.Android/Properties/AssemblyInfo.cs
+++ src/FloridaLottery.Android/Properties/AssemblyInfo.cs
@@ -16,15 +16,6 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
