cfe2a8be1fb01a900dbcfecdf6753faedd8ebf2b
Daniel Tovar <danieldaniyyelda@gmail.com>
2021-03-03 01:49:59 -0400

    config mobile build tools

---
+++ src/FloridaLottery/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FloridaLottery.Services;
+using FloridaLottery.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Analytics;
+
+namespace FloridaLottery
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start($"android={Helpers.Settings.AppCenterIdDroid}" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
